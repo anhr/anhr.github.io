ce6295dd87925848624d3e13542d3709b6b508ca
Andrej <anhr@mail.ru>
2017-11-21 18:41:44 +0700

    AboutMe

---
+++ AboutMe/deploy.bat
@@ -0,0 +1,10 @@
+rem run as administrator
+
+
+cd /D "G:\My documents\MyProjects\trunk\WebFeatures\WebFeatures\GoogleSite\AboutMe"
+set jsFolderDest=C:\inetpub\wwwroot\GoogleSite\AboutMe\
+
+xcopy index.html %jsFolderDest%index.html /Y
+
+pause
+
